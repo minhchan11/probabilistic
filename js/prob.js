579108ac62d2eeabb8b7b52ae02f4b0f1d90d8fd
Minh Phuong <mphuong@kent.edu>
2017-07-20 14:45:12 -0700

    Add css

--- js/prob.js
+++ js/prob.js
@@ -3,8 +3,14 @@ var canvas = d3.select("#d3")
               .attr("width",500)
               .attr("height",500);
 
-var circle = canvas.append("circle")
-                   .attr("cx",250)
-                   .attr("cy",250)
-                   .attr("r",50)
-                   .attr("fill","red");
+// var circle = canvas.append("circle")
+//                    .attr("cx",250)
+//                    .attr("cy",250)
+//                    .attr("r",200)
+//                    .attr("fill","red");
+
+// var rtg = canvas.append("rect")
+//                 .attr("x",300)
+//                 .attr("y",250)
+//                 .attr("width",100)
+//                 .attr("height",50)
