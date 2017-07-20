4ac842878d530026d8ce6db07c6ffb5ff7926680
Minh Phuong <mphuong@kent.edu>
2017-07-20 15:51:41 -0700

    Add graphs using d3

--- js/prob.js
+++ js/prob.js
@@ -1,16 +1,30 @@
+
+var dataArray = [5,20, 40, 50,60];
+var width = 500;
+var height = 500;
+
+
+
+var widthScale = d3.scaleLinear()
+                    .domain([0, 60])
+                    .range([0, width]);
+
+var color = d3.scaleLinear()
+                    .domain([0, 60])
+                    .range(["yellow", "blue"]);
+
 var canvas = d3.select("#d3")
-              .append("svg")
-              .attr("width",500)
-              .attr("height",500);
+               .append("svg")
+               .attr("width",width)
+               .attr("height",height)
+               .attr("transform","scale(1,-1)");
 
-// var circle = canvas.append("circle")
-//                    .attr("cx",250)
-//                    .attr("cy",250)
-//                    .attr("r",200)
-//                    .attr("fill","red");
 
-// var rtg = canvas.append("rect")
-//                 .attr("x",300)
-//                 .attr("y",250)
-//                 .attr("width",100)
-//                 .attr("height",50)
+var bars = canvas.selectAll("rect")
+                 .data(dataArray)
+                 .enter()
+                  .append("rect")
+                  .attr("height", function(d) {return widthScale(d);})
+                  .attr("width", 50)
+                  .attr("fill", function(d){ return color(d);})
+                  .attr("x", function(d,i) { return i * 100;});
